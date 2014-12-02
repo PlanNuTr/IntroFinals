1985bfb5d1b54e4bafb97a91eec7d0652bfa10d6
PlanNuTr <nuntuchaporn.plan@gmail.com>
2014-12-02 15:31:01 +0700

    Add == 0

--- lib/test.rb
+++ lib/test.rb
@@ -3,12 +3,13 @@ class Test
 		#This make you able to Test.new()
 	end
 	def lovemac(number)
-		if number % 3 
+		if number%3 == 0
 			puts "Love"
-		elsif number % 5
+		elsif number%5 == 0
 			puts "Mac"
-		elsif number % 15
+		elsif number%15 == 0
 			puts "HateWindows"
 		return number
+		end
 	end
 end
