ba46db268844a248540fc91347bbee4b92152352
PlanNuTr <nuntuchaporn.plan@gmail.com>
2014-12-02 15:54:19 +0700

    Add state

--- lib/test.rb
+++ lib/test.rb
@@ -1,15 +1,18 @@
 class Test
 	def initialize()  
-		#This make you able to Test.new()
+		@state = "Love"
 	end
 	def lovemac(number)
 		if number%3 == 0
+			@state = "Love"
 			puts "Love"
 		elsif number%5 == 0
+			@state = "Mac"
 			puts "Mac"
 		elsif number%15 == 0
+			@state = "Hate"
 			puts "HateWindows"
-		return number
+		return @state
 		end
 	end
 end
--- spec/lib/test_spec.rb
+++ spec/lib/test_spec.rb
@@ -3,12 +3,23 @@ require "spec_helper"
 
 describe Test do
 	describe "#lovemac(number)" do
-		#Describe Method
-		it "Do you love mac" do
-			#Describe method scenario
+		it "You love" do
 			test = Test.new()
-			state = test.lovemac(2);
-			expect(state).to eq(2)
+			state = test.lovemac(3);
+			expect(state).to eq("Love")
 		end
+
+		it "You love mac" do
+			test = Test.new()
+			state = test.lovemac(5);
+			expect(state).to eq("Mac")
+		end
+
+		it "You hate Windows" do
+			test = Test.new()
+			state = test.lovemac(15);
+			expect(state).to eq("Hate")
+		end
+
 	end
 end
